067e67d4940992b17fb666d504c7771e5380f22f
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-08-27 20:17:12 +0500

    add result file

---
+++ portfilo/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        './*.html',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'custom-blue': '#1e40af',
+            },
+            fontFamily: {
+                'sans': ['Helvetica', 'Arial', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
