e265c08b975a0d18b23b390081a8f56c03a7a7e2
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-08-29 02:10:20 +0500

    home page add new contant

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,10 +1,25 @@
-// src/components/Footer.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="bg-blue-600 text-white text-center py-4">
-      <p>&copy; 2024 [Your Name]. All rights reserved.</p>
+      <div className="flex justify-center space-x-6 mb-4">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+      </div>
+      <p>&copy; 2024 M Ali Anjum. All rights reserved.</p>
     </footer>
   );
 };
