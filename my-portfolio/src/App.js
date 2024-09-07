4a79723bbd4cdff3ae062a97931ece49dfeee52c
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-09-07 14:51:52 +0500

    add table

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,9 +5,12 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Contact from './components/Contact';
+import Contact from './components/Contact'
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Portfolio from "./components/Portfolio/Portfolio"
+import Resume from "./components/Resume/Resume"
+import Blog from "./components/Blog/Blog"
 
 function App() {
   return (
@@ -17,8 +20,18 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/Blog" element={<Blog />} />
+        
+       
       </Routes>
+    
+      <Portfolio />
+      <Resume />
+      <Blog />
+      
       <Footer />
     </Router>
   );
